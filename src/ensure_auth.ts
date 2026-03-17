dc690958865e3155650fa71b9cc15ee611c07594
ezShroom <68300730+ezShroom@users.noreply.github.com>
2026-03-17 22:35:02 +0000

    run poke from correct dir

--- src/ensure_auth.ts
+++ src/ensure_auth.ts
@@ -1,14 +1,17 @@
 import { $ } from 'bun'
+import path from 'node:path'
+
+const dir = path.join(import.meta.dirname, '../')
 
 export async function ensureAuth() {
 	while (true) {
 		try {
-			return (await $`bun poke whoami`.quiet()).text()
+			return (await $`bun poke whoami`.quiet().cwd(dir)).text()
 		} catch {
 			while (true) {
 				console.log('bunx poke login'.dim)
 				try {
-					await $`bun poke login`
+					await $`bun poke login`.cwd(dir)
 					break
 				} catch {
 					prompt(
